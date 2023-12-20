d32efb2c8da1dd934a46bae2d741b52e7e8659f3
Santo cyriac <132189839+Tw0side@users.noreply.github.com>
2023-12-20 11:28:33 +0530

    Create FileToConsole.java

---
+++ FileToConsole.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class FileToConsole
+{
+	public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String file;
+String str=null;
+System.out.println("Enter the file to be read");
+file=sc.nextLine();
+try
+{
+	FileReader fr=new FileReader(file);
+	BufferedReader br=new BufferedReader(fr);
+	System.out.println("The file contents are :");
+while((str=br.readLine())!=null)
+{
+	System.out.println(str);
+}
+br.close();
+}
+catch(IOException e)
+{
+	System.out.println("EXCEPTION CAUGHT"+e);
+}
+}
+}
